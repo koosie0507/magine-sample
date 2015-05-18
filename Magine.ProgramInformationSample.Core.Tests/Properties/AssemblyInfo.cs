0ddb7396706ec7b00b3b9fcbba769c6050e2575f
Andrei Olar <andrei.olar@intel.com>
2015-05-18 11:06:59 +0300

    Scaffolding for sample core
    Added scaffolding for the core logic.

---
+++ Magine.ProgramInformationSample.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Magine.ProgramInformationSample.Core.Tests")]
+[assembly: AssemblyDescription("Tests for the core logic of the program information sample.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Magine.ProgramInformationSample.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("23ab1086-ac59-465a-9c49-48a883ffd7ad")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Magine.ProgramInformationSample.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Magine.ProgramInformationSample.Core")]
+[assembly: AssemblyDescription("Core logic for the program information sample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Magine.ProgramInformationSample.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
